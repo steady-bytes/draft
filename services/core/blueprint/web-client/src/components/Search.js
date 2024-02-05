82f06acadf391597eb3efd8dad90d0dbeb55e7eb
Ian Miller (NLS) <ian.miller@nlsnow.com>
2024-02-05 12:06:42 -0600

    Search/Filter for Key Values

--- services/core/blueprint/web-client/src/components/Search.js
+++ services/core/blueprint/web-client/src/components/Search.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 
-const Search = ({ options, onSelect }) => {
+const Search = ({ placeholder, options, onSelect }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
   const dropdownRef = useRef(null);
@@ -24,13 +24,19 @@ const Search = ({ options, onSelect }) => {
   };
 
   const toggleDropdown = (event) => {
-    event.stopPropagation(); // Prevent the event from propagating to the parent div
+    event.stopPropagation();
     setIsOpen(!isOpen);
   };
 
   const handleOptionClick = (option) => {
     onSelect(option);
     setIsOpen(false);
+    setSearchTerm(option.label);
+  };
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+    setIsOpen(true);
   };
 
   const filteredOptions = options.filter((option) =>
@@ -38,27 +44,44 @@ const Search = ({ options, onSelect }) => {
   );
 
   return (
-    <div className="select">
+    <div className="search-container">
       <div className="selected-option" onClick={toggleDropdown}>
-        {searchTerm ? searchTerm : "Select..."}
+        <input
+          type="text"
+          className="search-input"
+          placeholder={placeholder}
+          value={searchTerm}
+          onChange={handleInputChange}
+          onClick={(e) => e.stopPropagation()}
+        />
         {isOpen ? (
-          <FaChevronUp onClick={toggleDropdown} />
+          <FaChevronUp
+            className="icon-search icon-search-up"
+            onClick={toggleDropdown}
+          />
         ) : (
-          <FaChevronDown onClick={toggleDropdown} />
+          <FaChevronDown
+            className="icon-search icon-search-down"
+            onClick={toggleDropdown}
+          />
         )}
       </div>
       {isOpen && (
         <div className="options" ref={dropdownRef}>
           <div className="dropdown-list">
-            {filteredOptions.map((option) => (
-              <div
-                key={option.value}
-                className="option"
-                onClick={() => handleOptionClick(option)}
-              >
-                {option.label}
-              </div>
-            ))}
+            {filteredOptions.length ? (
+              filteredOptions.map((option) => (
+                <div
+                  key={option.value}
+                  className="option"
+                  onClick={() => handleOptionClick(option)}
+                >
+                  {option.label}
+                </div>
+              ))
+            ) : (
+              <div className="option-none">No Matches Found</div>
+            )}
           </div>
         </div>
       )}
--- services/core/blueprint/web-client/src/pages/KeyValues.js
+++ services/core/blueprint/web-client/src/pages/KeyValues.js
@@ -48,19 +48,32 @@ export default function KeyValuesPage() {
   const options = [
     { label: "Option 1", value: "option1" },
     { label: "Option 2", value: "option2" },
-    { label: "Option 3", value: "option3" },
-    { label: "Option 4", value: "option4" },
-    { label: "Option 5", value: "option5" },
-    { label: "Option 6", value: "option6" },
+    { label: "Another Option", value: "anotherOption" },
+    { label: "Something Else", value: "somethingElse" },
+    {
+      label: "One more thing",
+      value: "oneMoreThing",
+    },
+    { label: "Another Thing", value: "anotherThing" },
+    { label: "Some Other Stuff", value: "someOtherStuff" },
     { label: "Option 7", value: "option7" },
     { label: "Option 8", value: "option8" },
-    { label: "Option 9", value: "option9" },
+    { label: "Another Option 2", value: "anotherOption2" },
   ];
 
   return (
     <div className="keyvalue-container">
-      <div>
-        <Search options={options} onSelect={handleSelect} />
+      <div className="keyvalue-search">
+        <Search
+          placeholder="Search Key/Value"
+          options={options}
+          onSelect={handleSelect}
+        />
+        {/* <Search
+          placeholder="Search Value"
+          options={options}
+          onSelect={handleSelect}
+        /> */}
       </div>
 
       <div className="card">
