f46ac9be0dc9d0643f8d56adc0b36c8ba0b97fac
Jack Kawell <jgkawell@outlook.com>
2024-07-19 17:11:57 -0500

    removed web-client as it's not used yet

--- services/core/fuse/web-client/src/components/MainContainer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../../public/globals.css";
-
-const MainContainer = ({ children }) => (
-  <div className="main-container">{children}</div>
-);
-
-export default MainContainer;
