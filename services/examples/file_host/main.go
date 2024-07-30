76f9dd527075ee6b5ffd497772d7adeb1a2ce862
Jack Kawell <jgkawell@outlook.com>
2024-07-30 14:06:11 -0500

    file_host example

--- services/examples/file_host/main.go
+++ services/examples/file_host/main.go
@@ -20,13 +20,13 @@ func main() {
 	defer chassis.New(logger).
 		WithClientApplication(files).
 		WithRoute(&ntv1.Route{
-			Name: "file_host",
-			// This is the host ip (IPv4) that the cluster will be forwarding the traffic to
-			// in my case I am using the host machine's IP address of my dev box.
-			Host: "10.0.0.108",
-			Port: 8080,
 			Match: &ntv1.RouteMatch{
 				Prefix: "/",
+				Host: "localhost",
+			},
+			Endpoint: &ntv1.Endpoint{
+				Host: "localhost",
+				Port: 8080,
 			},
 		}).
 		Start()
