99c5890ca56ca2057f340525d6681d787ab1a0f4
AndrewSC208 <meilingandrew@gmail.com>
2024-11-03 13:29:38 -0600

    A lock less write produce implementation. Locks are not needed when sending message from producers to consumers. A lock is only needed when setting up the channel the first time

--- services/core/catalyst/broker/controller.go
+++ services/core/catalyst/broker/controller.go
@@ -23,7 +23,10 @@ type (
 	}
 	atomicMap struct {
 		mu sync.RWMutex
-		m  map[string][]*connect.ServerStream[acv1.ConsumeResponse]
+		// Store the routine client connection
+		m map[string][]*connect.ServerStream[acv1.ConsumeResponse]
+		// yield the client connection to a thread, and then send events to it
+		n map[string]chan *acv1.Message
 	}
 
 	register struct {
@@ -36,17 +39,21 @@ func newState() *atomicMap {
 	return &atomicMap{
 		mu: sync.RWMutex{},
 		m:  make(map[string][]*connect.ServerStream[acv1.ConsumeResponse]),
+		n:  make(map[string]chan *acv1.Message),
 	}
 }
 
 func (am *atomicMap) Insert(key string, resStream *connect.ServerStream[acv1.ConsumeResponse]) {
-	am.mu.Lock()
-	defer am.mu.Unlock()
+	// TODO: Figure out how to start with a read lock?
+	am.mu.RLock()
+	defer am.mu.RUnlock()
 	list, ok := am.m[key]
 	if !ok {
+		am.mu.RUnlock()
+		am.mu.Lock()
+		defer am.mu.Unlock()
 		var list []*connect.ServerStream[acv1.ConsumeResponse]
 		list = append(list, resStream)
-
 		am.m[key] = list
 	} else {
 		list = append(list, resStream)
@@ -54,10 +61,42 @@ func (am *atomicMap) Insert(key string, resStream *connect.ServerStream[acv1.Con
 	}
 }
 
+func (am *atomicMap) Broker(key string, resStream *connect.ServerStream[acv1.ConsumeResponse]) {
+	am.mu.RLock()
+	defer am.mu.RUnlock()
+	ch, found := am.n[key]
+	if !found {
+		// create the channel to add to map
+		ch := make(chan *acv1.Message)
+
+		// store channel in map for future connections
+		am.mu.RUnlock()
+		am.mu.Lock()
+		am.n[key] = ch
+		am.mu.Unlock()
+
+		// now start a new routine and keep it open as long as the `ch` channel has connected clients
+		go send(ch, resStream)
+	} else {
+		// the channel is already made and shared with other consumers, and producers so we can just use `ch`
+		go send(ch, resStream)
+	}
+}
+
+func send(ch chan *acv1.Message, stream *connect.ServerStream[acv1.ConsumeResponse]) {
+	// when the channel receives a message send to the stream the client is holding onto
+	for {
+		m := <-ch
+		msg := &acv1.ConsumeResponse{
+			Message: m,
+		}
+		stream.Send(msg)
+	}
+}
+
 func (am *atomicMap) Broadcast(key string, msg *acv1.Message) {
 	am.mu.Lock()
 	defer am.mu.Unlock()
-
 	list, ok := am.m[key]
 	if ok {
 		res := &acv1.ConsumeResponse{
@@ -111,17 +150,35 @@ func (c *controller) produce(producerMsgChan chan *acv1.Message) {
 	}
 }
 
-func (c *controller) consume(reg chan register) {
+// consume - Will create a hash of the message domain, and typeUrl then save the msg.ServerStream to an `atomicMap`
+// that can be used to `Broadcast` messages to when a message is produced. Con's to this approach are a `RWMutex`
+// has to be used to `Broadcast` the message so the connected stream
+// func (c *controller) consume(reg chan register) {
+// 	for {
+// 		msg := <-reg
+// 		fmt.Print("Receive a request to setup a consumer", msg)
+
+// 		// make hash of <domain><msg.Type.String>
+// 		key := c.hash(msg.GetDomain(), msg.GetKind().GetTypeUrl())
+
+// 		// use hash as key if the hash does not exist, then create a slice of connections
+// 		// and append the connection to the slice
+//		c.state.Insert(key, msg.ServerStream)
+// 	}
+// }
+
+// consume - Will create a hash of the message domain, and typeUrl to use as a key to a tx, and rx sides of a channel
+// the `tx` or transmitter will be used when a producer produces an event to send the event to each client that is consuming
+// events of the domain, and typeUrl.
+func (c *controller) consume(registerChan chan register) {
 	for {
-		msg := <-reg
-		fmt.Print("Receive a request to setup a consumer", msg)
+		// create a shared channel that will receive any kind of message of that domain, and typeUrl
+		// add the receiver to a go routine that will keep the `ServerStream` open and send any messages
+		// received up to the client connect.
 
-		// make hash of <domain><msg.Type.String>
+		msg := <-registerChan
 		key := c.hash(msg.GetDomain(), msg.GetKind().GetTypeUrl())
-
-		// use hash as key if the hash does not exist, then create a slice of connections
-		// and append the connection to the slice
-		c.state.Insert(key, msg.ServerStream)
+		c.state.Broker(key, msg.ServerStream)
 	}
 }
 
