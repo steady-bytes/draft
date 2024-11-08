df77fa09672fc49d5032fe3414bf08e0541bd226
Jack Kawell <jack@kawell.us>
2024-11-08 15:59:14 -0600

    feat: implement Delete() on key/value in blueprint

--- services/core/blueprint/key_value/rpc.go
+++ services/core/blueprint/key_value/rpc.go
@@ -3,7 +3,6 @@ package key_value
 import (
 	"context"
 	"errors"
-	"fmt"
 	"strings"
 	"time"
 
@@ -35,8 +34,10 @@ func NewRPC(logger chassis.Logger, controller Controller) Rpc {
 }
 
 var (
-	ErrFailedSet  = errors.New("failed to set key/value pair")
-	ErrFailedList = errors.New("failed to list all values for provided kind")
+	ErrFailedSet    = errors.New("failed to set key/value pair")
+	ErrFailedList   = errors.New("failed to list all values for provided kind")
+	ErrFailedDelete = errors.New("failed to delete by key for provided kind")
+	ErrFailedGet    = errors.New("failed to get value for key")
 )
 
 func (h *rpc) RegisterRPC(server chassis.Rpcer) {
@@ -46,22 +47,18 @@ func (h *rpc) RegisterRPC(server chassis.Rpcer) {
 
 func (h *rpc) Set(ctx context.Context, req *connect.Request[kvv1.SetRequest]) (*connect.Response[kvv1.SetResponse], error) {
 	var (
-		logger = h.logger.WithContext(ctx)
-		key    = strings.TrimSpace(req.Msg.GetKey())
-		value  = req.Msg.GetValue()
-		err    error
+		log   = h.logger.WithContext(ctx)
+		key   = strings.TrimSpace(req.Msg.GetKey())
+		value = req.Msg.GetValue()
 	)
 
-	_, err = h.controller.Set(logger, key, value, 500*time.Millisecond)
+	_, err := h.controller.Set(log, key, value, 500*time.Millisecond)
 	if err != nil {
-		logger.
-			WithError(err).
-			Error(ErrFailedSet.Error())
-
-		return nil, err
+		log.WithError(err).Error(ErrFailedSet.Error())
+		return nil, ErrFailedSet
 	}
 
-	logger.WithField("key", key).Info("value saved")
+	log.WithField("key", key).Info("value saved")
 
 	return connect.NewResponse(&kvv1.SetResponse{
 		Key: key,
@@ -77,8 +74,8 @@ func (h *rpc) Get(ctx context.Context, req *connect.Request[kvv1.GetRequest]) (*
 
 	value, err := h.controller.Get(log, key, value)
 	if err != nil {
-		fmt.Println("error reading: ", err)
-		return nil, errors.New("failed to get value for key")
+		log.WithError(err).Error(ErrFailedGet.Error())
+		return nil, ErrFailedGet
 	}
 
 	return connect.NewResponse(&kvv1.GetResponse{
@@ -87,18 +84,32 @@ func (h *rpc) Get(ctx context.Context, req *connect.Request[kvv1.GetRequest]) (*
 }
 
 func (h *rpc) Delete(ctx context.Context, req *connect.Request[kvv1.DeleteRequest]) (*connect.Response[kvv1.DeleteResponse], error) {
-	return nil, errors.New("not implemented")
+	var (
+		log   = h.logger.WithContext(ctx)
+		key   = strings.TrimSpace(req.Msg.GetKey())
+		value = req.Msg.GetValue()
+	)
+
+	err := h.controller.Delete(log, key, value)
+	if err != nil {
+		h.logger.WithError(err).Error(ErrFailedDelete.Error())
+		return nil, ErrFailedDelete
+	}
+
+	return connect.NewResponse(&kvv1.DeleteResponse{
+		Key: key,
+	}), nil
 }
 
 func (h *rpc) List(ctx context.Context, req *connect.Request[kvv1.ListRequest]) (*connect.Response[kvv1.ListResponse], error) {
 	var (
-		logger = h.logger.WithContext(ctx)
-		kind   = req.Msg.GetValue()
+		log  = h.logger.WithContext(ctx)
+		kind = req.Msg.GetValue()
 	)
 
-	valuesMap, err := h.controller.List(logger, kind)
+	valuesMap, err := h.controller.List(log, kind)
 	if err != nil {
-		fmt.Println(err)
+		log.WithError(err).Error(ErrFailedList.Error())
 		return nil, ErrFailedList
 	}
 
